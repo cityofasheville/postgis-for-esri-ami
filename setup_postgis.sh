eb6060ab510674721073b4a5fabe4d497dfed230
Dave Michelson <daveism@gmail.com>
2014-08-22 10:37:27 -0400

    Update setup_postgis.sh

--- setup_postgis.sh
+++ setup_postgis.sh
@@ -42,12 +42,12 @@ sudo service postgresql start
 
 #spatially  enable default databases make sure that the user postgres is set to trust for local conections in the pg_hba.conf file
 #then make sure you switch this to md5 after.
-psql -c "psql -U postgres -d geodata -c \"create extension postgis;\""
-psql -c "psql -U postgres -d geodata -c \"create extension postgis_toplogy;\""
-psql -c "psql -U postgres -d geodata -c \"create extension hstore;\""
-psql -c "psql -U postgres -d egdb -c \"create extension postgis;\""
-psql -c "psql -U postgres -d egdb -c \"create extension postgis_toplogy;\""
-psql -c "psql -U postgres -d egdb -c \"create extension hstore;\""
+psql -U postgres -d geodata -c "create extension postgis;"
+psql -U postgres -d geodata -c "create extension postgis_toplogy;"
+psql -U postgres -d geodata -c "create extension hstore;"
+psql -U postgres -d egdb -c "create extension postgis;"
+psql -U postgres -d egdb -c "create extension postgis_toplogy;"
+psql -U postgres -d egdb -c "create extension hstore;"
 
 
 sudo service postgresql stop
