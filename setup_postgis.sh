b9c63959f360adf3c417afce59186729b9cc0264
Dave Michelson <daveism@gmail.com>
2014-08-22 09:27:46 -0400

    add make installs and set up postgres for postgis

--- setup_postgis.sh
+++ setup_postgis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#update and make sure all postgis depencies are setup
 apt-get -y update
 apt-get -y install wget
 wget --quiet --no-check-certificate -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
@@ -13,21 +14,42 @@ apt-get -y install libxml2-dev
 apt-get -y install libproj-dev
 apt-get -y install libjson0-dev
 apt-get -y install libgdal1-dev
+
+#get configure, make, and install geos.
 cd ~
 wget http://download.osgeo.org/geos/geos-3.3.9.tar.bz2
 tar xfj geos-3.3.9.tar.bz2
 cd geos-3.3.9
 ./configure
-make
-sudo make install
-cd ..
+sudo make -s
+sudo make install -s
 
+#get configure, make, and install postgis with raster and topology
+cd ~
 wget http://download.osgeo.org/postgis/source/postgis-2.0.6.tar.gz
 tar xfz postgis-2.0.6.tar.gz
 cd postgis-2.0.6
-
 ./configure
-make
-sudo make install
+make -s 
+sudo make install -s 
 sudo ldconfig
-sudo make comments-install
+sudo make comments-install -s 
+sudo ldconfig
+
+#open connections for all postgres 
+echo "listen_addresses = '*'" >> /etc/postgresql/9.2/main/postgresql.conf
+
+#set permissions for all connections
+echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/9.2/main/pg_hba.conf
+echo "local    all            postgres                                trust" >> /etc/postgresql/9.2/main/pg_hba.conf
+
+service postgresql stop
+service postgresql start 
+
+#spatial enable default databases
+postgres -c "psql postgres -d geodata -c \"create extension postgis;\""
+postgres -c "psql postgres -d geodata -c \"create extension postgis_toplogy;\""
+postgres -c "psql postgres -d geodata -c \"create extension hstore;\""
+postgres -c "psql postgres -d egdb -c \"create extension postgis;\""
+postgres -c "psql postgres -d egdb -c \"create extension postgis_toplogy;\""
+postgres -c "psql postgres -d egdb -c \"create extension hstore;\""
