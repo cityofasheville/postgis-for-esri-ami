68898c72b477931b996f79ac0ba8cd5d874531bc
Dave Michelson <daveism@gmail.com>
2014-08-22 10:11:06 -0400

    Update setup_postgis.sh

--- setup_postgis.sh
+++ setup_postgis.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 #update and make sure all postgis depencies are setup
-apt-get -y update
-apt-get -y install wget
+sudo apt-get -y update
+sudo apt-get -y install wget
 wget --quiet --no-check-certificate -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list
-apt-get -y update
-apt-get -y install postgresql-server-dev-9.2
-apt-get -y install postgresql-server-contrib-9.2
-apt-get -y install libproj0
-apt-get -y install build-essential
-apt-get -y install libxml2-dev
-apt-get -y install libproj-dev
-apt-get -y install libjson0-dev
-apt-get -y install libgdal1-dev
+sudo apt-get -y update
+sudo apt-get -y install postgresql-server-dev-9.2
+sudo apt-get -y install postgresql-server-contrib-9.2
+sudo apt-get -y install libproj0
+sudo apt-get -y install build-essential
+sudo apt-get -y install libxml2-dev
+sudo apt-get -y install libproj-dev
+sudo apt-get -y install libjson0-dev
+sudo apt-get -y install libgdal1-dev
 
 #get configure, make, and install geos.
 cd ~
@@ -43,8 +43,8 @@ echo "listen_addresses = '*'" >> /etc/postgresql/9.2/main/postgresql.conf
 echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/9.2/main/pg_hba.conf
 echo "local    all            postgres                                trust" >> /etc/postgresql/9.2/main/pg_hba.conf
 
-service postgresql stop
-service postgresql start 
+sudo service postgresql stop
+sudo service postgresql start 
 
 #spatial enable default databases
 postgres -c "psql postgres -d geodata -c \"create extension postgis;\""
