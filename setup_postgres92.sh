c0e44d3255b447c7d64291a5bd3b696b8d8cd027
Dave Michelson <daveism@gmail.com>
2014-08-22 13:01:23 -0400

    Update setup_postgres92.sh

--- setup_postgres92.sh
+++ setup_postgres92.sh
@@ -2,8 +2,8 @@
 #also for loading postgres 9.2 for other installs
 cd /etc/apt/sources.list.d/
 sudo touch pdgd.list
-sudo chmod 777 pdgd.list
-echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >>  pdgd.list
+#sudo chmod 777 pdgd.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >>  sudo -u root pdgd.list
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install postgresql-9.2
