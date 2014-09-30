d3ba3e394fa463498b9b238f4f3a09f8d6dd7cde
Dave Michelson <daveism@gmail.com>
2014-09-30 15:43:35 -0400

    fixed copypaste issues

--- addnewsde.py
+++ addnewsde.py
@@ -1,6 +1,5 @@
 #import arcpy module
 import arcpy
 
-arcpy.CreateEnterpriseGeodatabase_management(database_platform="PostgreSQL",instance_name="localhost",database_name="coagis",account_authentic
-ation="DATABASE_AUTH",database_admin="postgres",database_admin_password="postgres",sde_schema="SDE_SCHEMA",gdb_admin_name="sde",gdb_admin_pass
-word="E$ri3774",tablespace_name="#",authorization_file="/home/arcgis/undefined-2017.respc")
+#License file is specific for city of avl.  you will need to acquire your own license file and change the database_name.  this is for automation.
+arcpy.CreateEnterpriseGeodatabase_management(database_platform="PostgreSQL",instance_name="localhost",database_name="coagis",account_authentication="DATABASE_AUTH",database_admin="postgres",database_admin_password="postgres",sde_schema="SDE_SCHEMA",gdb_admin_name="sde",gdb_admin_password="E$ri3774",tablespace_name="#",authorization_file="/home/arcgis/undefined-2017.respc")
