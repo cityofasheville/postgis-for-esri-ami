476f2cf91d81d6e4260b284d574ed88f0ddd9a5c
Ubuntu <ubuntu@ip-10-5-169-99.(none)>
2014-10-14 12:57:48 +0000

    fixed bad database ref

--- setup_postgis_owner.sh
+++ setup_postgis_owner.sh
@@ -5,10 +5,10 @@ sudo service postgresql stop
 sudo service postgresql start 
 
 sudo -u postgres PGPASSWORD=postgres psql -U postgres -d coagis -c "CREATE SCHEMA owner AUTHORIZATION owner;"
-sudo -u postgres PGPASSWORD=postgres psql -U postgres -d template_postgis -c "GRANT ALL ON SCHEMA owner TO sde;"
-sudo -u postgres PGPASSWORD=postgres psql -U postgres -d template_postgis -c "GRANT ALL ON SCHEMA owner TO public;"
-sudo -u postgres PGPASSWORD=postgres psql -U postgres -d template_postgis -c "GRANT ALL ON SCHEMA owner to postgres;"
-sudo -u postgres PGPASSWORD=postgres psql -U postgres -d template_postgis -c "GRANT ALL ON DATABASE coagis to owner;""
+sudo -u postgres PGPASSWORD=postgres psql -U postgres -d coagis -c "GRANT ALL ON SCHEMA owner TO sde;"
+sudo -u postgres PGPASSWORD=postgres psql -U postgres -d coagis -c "GRANT ALL ON SCHEMA owner TO public;"
+sudo -u postgres PGPASSWORD=postgres psql -U postgres -d coagis -c "GRANT ALL ON SCHEMA owner to postgres;"
+sudo -u postgres PGPASSWORD=postgres psql -U postgres -d coagis -c "GRANT ALL ON DATABASE coagis to owner;""
 
 sudo service postgresql stop
 sudo service postgresql start 
